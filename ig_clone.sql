767e46cae062bfe3d99e429658185cbbe4edf0da
Romina 로미나 <83961643+rominalodolo@users.noreply.github.com>
2026-02-17 12:00:21 +0200

    Create ig_clone.sql

---
+++ ig_clone.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE instagramcloneapp_db;
+
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
